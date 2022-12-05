206f36e2e0bd1369b64f6a6a59d80bad7d20c26c
Сергей <100863938+Sergei190@users.noreply.github.com>
2022-12-05 15:50:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Sergey";
+    String surname = "Kudryashov";
+    
+    double income = 48.000;
+    double spending = 12.000;
+    double surplus = income / spending;
+    System.out.println(name + " " + surname + ", месячный остаток:" + surplus);
+  }
+}
